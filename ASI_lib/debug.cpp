db8372ef510446d5b91c53253e750f52eb8bac1b
cpast <8211353+cpast@users.noreply.github.com>
2020-08-19 12:58:49 -0400

    fix some stuff

--- ASI_lib/debug.cpp
+++ ASI_lib/debug.cpp
@@ -36,6 +36,7 @@ void cleanup_log()
 
 #else
 #include <strsafe.h>
+#include <cassert>
 
 HANDLE file = INVALID_HANDLE_VALUE;
 bool setup_attempted = false;
@@ -54,6 +55,9 @@ bool setup_log()
 		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), 0, (LPTSTR)&errorMessage, 0, NULL);
 		SIZE_T msgBoxMessageSize = sizeof(TCHAR) * ((SIZE_T)lstrlen(errorMessage) + (SIZE_T)lstrlen(msgBoxPrefix) + 1);
 		msgBoxMessage = (LPTSTR) LocalAlloc(LMEM_ZEROINIT, msgBoxMessageSize);
+		if (msgBoxMessage == NULL) {
+			return false;
+		}
 		StringCchCat(msgBoxMessage, msgBoxMessageSize, msgBoxPrefix);
 		StringCchCat(msgBoxMessage, msgBoxMessageSize, errorMessage);
 		MessageBox(NULL, msgBoxMessage, TEXT("Error"), MB_OK);
@@ -68,9 +72,11 @@ void log(const char* fmt ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	if (file == INVALID_HANDLE_VALUE)
+	if (file == INVALID_HANDLE_VALUE || file == 0)
 		if (!setup_log())
 			return;
+	if (file == INVALID_HANDLE_VALUE || file == 0)
+		return;
 	char* outputString = NULL;
 	SIZE_T outputLen = vsnprintf(outputString, 0, fmt, args);
 	outputString = (char*)LocalAlloc(LMEM_ZEROINIT, outputLen + 1);
